04b17d34f2b5bc4a0bb6485a86eaf100b695d02b
Aaron Gibson <Aaron.Gibson@IRIS.CO.UK>
2019-11-26 16:17:31 +0000

    Improved the service worker fetch response

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ const precacheResources = [
 ];
 
 self.addEventListener('install', event => {
-  console.log('Service worker install event!');
+  console.log('Service worker install event activated!');
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => {
@@ -18,17 +18,29 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  console.log('Service worker activate event!');
+  console.log('Service worker activate event fired!');
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(caches.match(event.request)
-    .then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return fetch(event.request);
-      })
-    );
+  console.log('Fetch triggered', event);
+  // Let the browser do its default thing
+  // for non-GET requests.
+  if (event.request.method != 'GET') return;
+
+  // Prevent the default, and handle the request ourselves.
+  event.respondWith(async function() {
+    // Try to get the response from a cache.
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(event.request);
+
+    if (cachedResponse) {
+      // If we found a match in the cache, return it, but also
+      // update the entry in the cache in the background.
+      event.waitUntil(cache.add(event.request));
+      return cachedResponse;
+    }
+
+    // If we didn't find a match in the cache, use the network.
+    return fetch(event.request);
+  }());
 });
